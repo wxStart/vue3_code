caac5153ba396e2fe6789d7a004344c73d412449
vvyx_1024 <vvyx@qq.com>
2025-03-12 21:22:27 +0800

    自己实现render把虚拟节点 渲染成真实节点

--- packages/runtime-dom/dist/runtime-dom.js
+++ packages/runtime-dom/dist/runtime-dom.js
@@ -484,23 +484,76 @@ function patchProp(el, key, prevValue, nextValue) {
   }
 }
 
+// packages/runtime-core/src/index.ts
+function createRenderer(renderOptions2) {
+  const {
+    insert: hostInsert,
+    remove: hostRemove,
+    createElement: hostCreateElement,
+    createText: hostCreateText,
+    setText: hostSetText,
+    setElementText: hostSetElementText,
+    parentNode: hostParentNode,
+    nextSibling: hostNextSibling,
+    patchProp: hostPatchProp
+  } = renderOptions2;
+  const mountChildren = (children, container) => {
+    for (let index = 0; index < children.length; index++) {
+      patch(null, children[index], container);
+    }
+  };
+  const mountElement = (vnode, container) => {
+    console.log("vnode: ", vnode);
+    const { type, children, props, shapeFlag } = vnode;
+    let el = hostCreateElement(type);
+    if (props) {
+      for (const key in props) {
+        hostPatchProp(el, key, null, props[key]);
+      }
+    }
+    if (shapeFlag & 8 /* TEXT_CHILDREN */) {
+      debugger;
+      hostSetElementText(el, children);
+    } else if (shapeFlag & 16 /* ARRAY_CHILDREN */) {
+      mountChildren(children, el);
+    }
+    hostInsert(el, container);
+  };
+  const patch = (n1, n2, container) => {
+    if (n1 == n2) {
+      return;
+    }
+    if (n1 === null) {
+      mountElement(n2, container);
+    }
+  };
+  const render2 = (vnode, container) => {
+    patch(container._vnode || null, vnode, container);
+    container._vnode = vnode;
+  };
+  return {
+    render: render2
+  };
+}
+
 // packages/runtime-dom/src/index.ts
 var renderOptions = Object.assign(nodeOps, {
   patchProp
 });
-function createRenderer() {
-}
+var render = (vnode, container) => {
+  return createRenderer(renderOptions).render(vnode, container);
+};
 export {
   ReactiveEffect,
   activeEffet,
   computed,
-  createRenderer,
   effect,
   isReactive,
   isRef,
   proxyRefs,
   reactive,
   ref,
+  render,
   renderOptions,
   toReactive,
   toRef,
--- packages/runtime-dom/src/index.ts
+++ packages/runtime-dom/src/index.ts
@@ -3,7 +3,14 @@ export * from '@vue/reactivity';
 import { nodeOps } from './nodeOps';
 import { patchProp } from './patchProp';
 
+import { createRenderer } from '@vue/runtime-core';
+
 export const renderOptions = Object.assign(nodeOps, {
   patchProp,
 });
-export function createRenderer() {}
+// export function createRenderer() {}
+
+export const render = (vnode, container) => {
+  return createRenderer(renderOptions).render(vnode, container);
+};
+export * from '@vue/reactivity';
