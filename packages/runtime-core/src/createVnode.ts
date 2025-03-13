b132423808a3ab411c0c0d5b35b060aa665bfc5c
vvyx_1024 <vvyx@qq.com>
2025-03-13 22:08:59 +0800

    比较元素

--- packages/runtime-core/src/createVnode.ts
+++ packages/runtime-core/src/createVnode.ts
@@ -1,28 +1,32 @@
-import { isString, ShapeFlags } from "@vue/shared";
+import { isString, ShapeFlags } from '@vue/shared';
 
 export function isVnode(value) {
-    return value?.__v_isVnode;
-  }
-  
-  export function createVnode(type, props, children?) {
-      const shapeFlag = isString(type) ? ShapeFlags.ELEMENT : 0;
-    
-      const vnode = {
-        __v_isVnode: true,
-        type,
-        props,
-        children,
-        key: props?.key,
-        el: null,
-        shapeFlag,
-      };
-      if (children) {
-        if (Array.isArray(children)) {
-          vnode.shapeFlag |= ShapeFlags.ARRAY_CHILDREN;
-        } else {
-          vnode.children = String(children);
-          vnode.shapeFlag |= ShapeFlags.TEXT_CHILDREN;
-        }
-      }
-      return vnode;
+  return value?.__v_isVnode;
+}
+
+export function isSameVnode(n1, n2) {
+  return n1.type === n2.type && n1.key === n1.key;
+}
+
+export function createVnode(type, props, children?) {
+  const shapeFlag = isString(type) ? ShapeFlags.ELEMENT : 0;
+
+  const vnode = {
+    __v_isVnode: true,
+    type,
+    props,
+    children,
+    key: props?.key,
+    el: null,
+    shapeFlag,
+  };
+  if (children) {
+    if (Array.isArray(children)) {
+      vnode.shapeFlag |= ShapeFlags.ARRAY_CHILDREN;
+    } else {
+      vnode.children = String(children);
+      vnode.shapeFlag |= ShapeFlags.TEXT_CHILDREN;
     }
+  }
+  return vnode;
+}
--- packages/runtime-core/src/renderer.ts
+++ packages/runtime-core/src/renderer.ts
@@ -1,4 +1,5 @@
 import { ShapeFlags } from '@vue/shared';
+import { isSameVnode } from './createVnode';
 
 export function createRenderer(renderOptions) {
   const {
@@ -22,8 +23,9 @@ export function createRenderer(renderOptions) {
   const mountElement = (vnode, container) => {
     console.log('vnode: ', vnode);
     const { type, children, props, shapeFlag } = vnode;
-
-    let el = hostCreateElement(type);
+    // 让虚拟节点和真实节点产生关联
+    // 下次渲染可以继续使用郑愕节点
+    let el = (vnode.el = hostCreateElement(type));
     if (props) {
       for (const key in props) {
         hostPatchProp(el, key, null, props[key]);
@@ -48,18 +50,69 @@ export function createRenderer(renderOptions) {
     hostInsert(el, container);
   };
 
+  const processElement = (n1, n2, container) => {
+    // n1 是null 说明是初次渲染
+    if (n1 === null) {
+      mountElement(n2, container);
+    } else {
+      patchElement(n1, n2, container);
+    }
+  };
+
+  const patchProps = (oldProps, newProps, el) => {
+    for (const key in newProps) {
+      hostPatchProp(el, key, oldProps[key], newProps[key]);
+    }
+    for (const key in oldProps) {
+      if (!(key in newProps)) {
+        hostPatchProp(el, key, oldProps[key], null);
+      }
+    }
+  };
+
+  const patchChildren = (n1, n2, container){};
+
+  const patchElement = (n1, n2, container) => {
+    /*
+    1.比较元素的差异，肯定需要复用dom
+     2.比较属性
+    3.比较子节点
+    */
+
+
+     // 1.比较元素的差异，肯定需要复用dom
+    let el = (n2.el = n1.el); // dom复用
+
+    let oldProps = n1.props || {};
+    let newProps = n2.props || {};
+   // 2.比较属性
+    patchProps(oldProps, newProps, el);
+    // 3.比较子节点
+    // todo  这次写到比较孩子的函数入口
+    patchChildren(n1, n2, container);
+  };
   const patch = (n1, n2, container) => {
     if (n1 == n2) {
       return;
     }
-
-    // n1 是null 说明是初次渲染
-    if (n1 === null) {
-      mountElement(n2, container);
+    if (n1 && !isSameVnode(n1, n2)) {
+      unmount(n1);
+      n1 = null; // 这样就是直接走下面 mountElement 直接初次渲染n2
     }
+    processElement(n1, n2, container);
+  };
+
+  const unmount = (vnode) => {
+    hostRemove(vnode.el);
   };
 
   const render = (vnode, container) => {
+    if (vnode == null) {
+      if (container._vnode) {
+        // 第二次传入的节点是null 说明要移除改容器中的dom元素
+        unmount(container._vnode);
+      }
+    }
     // 将虚拟节点变成真实节点
     // console.log('vnode, container: ', vnode, container);
     patch(container._vnode || null, vnode, container);
